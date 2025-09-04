cfd1122bfe9b1e683fa9605bdadb5b63daaed88e
Meme Man <81907436+RaveTr@users.noreply.github.com>
2025-09-04 14:28:09 +0300

    Test code cleanup

--- forge/src/main/java/com/mememan/liveplayerreaction/LivePlayerReactionForge.java
+++ forge/src/main/java/com/mememan/liveplayerreaction/LivePlayerReactionForge.java
@@ -1,8 +1,5 @@
 package com.mememan.liveplayerreaction;
 
-import net.minecraft.data.DataGenerator;
-import net.minecraft.data.PackOutput;
-import net.minecraftforge.data.event.GatherDataEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
@@ -11,14 +8,5 @@ public class LivePlayerReactionForge {
     
     public LivePlayerReactionForge(FMLJavaModLoadingContext ctx) {
         LivePlayerReaction.initialize();
-
-        ctx.getModEventBus().addListener(this::data);
-    }
-
-    private void data(GatherDataEvent e) {
-        DataGenerator gen = e.getGenerator();
-        PackOutput output = gen.getPackOutput();
-
-        gen.addProvider(e.includeClient(), new TestProvider(output));
     }
 }
